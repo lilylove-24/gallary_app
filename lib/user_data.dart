8c33c8c2b529ce662b4970c52bb265f7f36ac933
lilylove-24 <aniewulilian0@gmail.com>
2021-09-16 22:45:16 -0600

    final gallary app project

---
+++ lib/user_data.dart
@@ -0,0 +1,23 @@
+List userData = [
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_1.jpg",
+    "userPost": "images/picture_1.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_2.jpg",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "23",
+  },
+  {
+    "userName": "Kwame Jantua",
+    "userLocation": "Adisadel Estate",
+    "userProfileImage": "images/profile_3.jpg",
+    "userPost": "images/picture_3.jfif",
+    "userPostLikes": "23",
+  }
+];
